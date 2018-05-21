e157cd05944b5a99a1dffe070ad5fdc1bdf76efc
alex yungan <yungan@hotmail.es>
2018-05-21 15:19:54 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
-
+import {
+  Component,
+  OnInit,
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+} from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'app';
+  imgbase64 = '';
+  imageData: any;
+  sanitzedImageData: any;
+  file: File;
+  constructor(private sanitizer: DomSanitizer) { }
+  public ngOnInit() {
+    
+this.getBase64(this.imgbase64);
+  }
+  getBase64(file) {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = ()=> {
+      console.log(reader.result);
+      this.imageData=reader.result;
+    };
+    reader.onerror =  (error)=> {
+      console.log('Error: ', error);
+    };
+  }
+  changeFile(event){
+    
+    let file=event.target.files[0];//archivo q selecciono
+    this.getBase64(file);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,//secesario par subir archivos
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
